b66cf13381e4e4a2ded2a60fe79ecf593f0803f2
arvindfalake-debug <arvindfalake@gmail.com>
2026-02-05 19:42:28 +0530

    Add random movement to 'no' button on mouseover

---
+++ Script.js
@@ -0,0 +1,9 @@
+const noBtn = document.getElementById("noBtn");
+
+noBtn.addEventListener("mouseover", () => {
+  const x = Math.random() * (window.innerWidth - noBtn.clientWidth);
+  const y = Math.random() * (window.innerHeight - noBtn.clientHeight);
+
+  noBtn.style.left = `${x}px`;
+  noBtn.style.top = `${y}px`;
+});
